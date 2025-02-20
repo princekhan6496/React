6b8e3ef1fccb7102208f4446f607591fcf891634
princekhan6496 <kingfishan786@gmail.com>
2025-02-21 00:05:34 +0530

    changes

---
+++ Tailwindcss/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css' 
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-500'>New starting with git and React</h1>
+      <h2 className='bg-yellow-600'>Tailwind test</h2>
+    </>
+  )
+}
+
+export default App
